2f2645d755c9110b995033d0381c31d1258a4950
lvsz <e@levi.si>
2017-03-02 11:31:20 +0100

    Clean-up and documentation

--- src/main.c
+++ src/main.c
@@ -1,6 +1,3 @@
-#include <stdlib.h>
-#include <stdio.h>
-
 #include "window.h"
 #include "game.h"
 
@@ -8,11 +5,16 @@ int main(int argc, char *argv[])
 {
     Game *game;
     size_t game_width, game_height;
+
+    /* if arguments are given, start in custom field
+     * otherwise load level 1 and start there */
     if (argc >= 3) {
         game_width = atoi(argv[1]);
         game_height = atoi(argv[2]);
         game = game_init(game_width, game_height, -1);
     } else {
+        /* when loading a level, width and height parameters
+         * are 0 to avoid allocating until reading the file */
         game = game_init(0, 0, 1);
         game_width = game->width;
         game_height = game->height;
@@ -20,10 +22,9 @@ int main(int argc, char *argv[])
 
     window_init(game_width, game_height);
 
-    puts("starting game");
+    /* keep game running until it returns 0 */
     while(run_game(game));
 
-    puts("exiting");
     free_game(game);
     return 0;
 }
